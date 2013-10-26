c67077f70c2ecfa233eebf5679a451eaea435737
Alexander Moriarty <alexander@dal.ca>
2013-10-26 14:34:19 +0200

    Initial commit
    rough draft.

---
+++ js/config.js
@@ -0,0 +1,16 @@
+var courses = [
+  {"title":"math",
+   "students":[
+   { "firstName":"John" , "lastName":"Doe", "uid": 1}, 
+   { "firstName":"Anna" , "lastName":"Smith", "uid": 2 }, 
+   { "firstName":"Peter" , "lastName":"Jones", "uid": 3 }
+   ]
+  },
+  {"title":"english",
+   "students":[
+   { "firstName":"John" , "lastName":"Doe", "uid": 1 }, 
+   { "firstName":"Anna" , "lastName":"Smith", "uid": 2 }, 
+   { "firstName":"Peter" , "lastName":"Jones", "uid": 3}
+   ]
+  }
+]
